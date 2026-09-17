828e83217666c579b691e826ccd0ae81420ccb2c
zuiderwijk <159496541+zuiderwijk@users.noreply.github.com>
2026-09-17 23:15:37 +0200

    Fix Per zender clipped current layer typing

--- features/guide/PerChannelGuideView.tsx
+++ features/guide/PerChannelGuideView.tsx
@@ -855,7 +855,11 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
   },
   currentVisualLayer: {
-    ...StyleSheet.absoluteFillObject,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
     overflow: 'hidden',
   },
   standardTimeCell: {
